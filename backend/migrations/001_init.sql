2a6df21facc1a85629055265ca4db312541eb9ee
Atulya <jaiswalatulya741@gmail.com>
2026-08-28 19:47:26 +0530

    wrote sql migration

---
+++ backend/migrations/001_init.sql
@@ -0,0 +1,22 @@
+ 
+CREATE TABLE raw_documents (
+  id SERIAL PRIMARY KEY,
+  source TEXT NOT NULL,
+  source_id TEXT NOT NULL,
+  raw_data JSONB NOT NULL,
+  fetched_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+CREATE TABLE documents (
+    id SERIAL PRIMARY KEY,
+    source TEXT NOT NULL,
+    source_id TEXT NOT NULL,
+    url TEXT,
+    title TEXT NOT NULL,
+    body TEXT NOT NULL,
+    author TEXT,
+    created_at TIMESTAMPTZ,
+    updated_at TIMESTAMPTZ,
+    metadata JSONB,
+    UNIQUE (source, source_id)
+)
